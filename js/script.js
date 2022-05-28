58427ef182922f225b2fa49f354996e24f3e0b4d
Wallisonfer <wallisonsp.santos@gmail.com>
2022-05-28 20:43:17 -0300

    Validando campo e-mail com JS

--- js/script.js
+++ js/script.js
@@ -1,15 +1,29 @@
 
 
 const botaoSubmit = document.getElementById('btnEnviarId')
+const validacaoEmail = document.getElementById('emailUserId')
 
+/* Evento para validação se foi digitado algum caracter */
 botaoSubmit.addEventListener("click", ()=>{
-    const campos = document.querySelectorAll("input[type='text'],input[type='email'],input[type='number'],input[type='date'], textarea")
+    const campos = document.querySelectorAll("input[type='text'],input[type='number'],input[type='date'], textarea")
 
     for (let x = 0; x < campos.length ; x++){
         if (campos[x].value == ""){
             alert("O CAMPO " + campos[x].placeholder + " não foi preenchido!")
         }
+        
     }
+    if (validacaoEmail.value == "" || validacaoEmail.value.indexOf('@')==-1 && validacaoEmail.value.indexOf('.')==-1){
+        alert( "Por favor, informe um E-MAIL válido!" );
+               return false
+    }   
 
     document.querySelector('.cadastro').submit()
+    return true
+    
 })
+
+/* Evento para validação do e-mail */
+
+
+ 
